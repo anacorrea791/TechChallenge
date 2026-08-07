9c52e2f326f0f2e210c71c4c1f1563e9e980a81f
anacorrea791 <correaanaclara@gmail.com.br>
2026-08-07 16:17:23 -0300

    Decorando as paginas e acresentando authorize no codigo

--- Controllers/AlunosEquipesController.cs
+++ Controllers/AlunosEquipesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using TechChallenge.Models;
 
 namespace TechChallenge.Controllers
 {
+     [Authorize] //Proteger as controller AlunosController, usando o atributo [Authorize],configurando autentificação/autorização.
     public class AlunosEquipesController : Controller
     {
         private readonly AppDbContext _context;
